1e1ce6fc2e18f86f31cbf02722e1a1c4ddc6960f
PrisicllaX <grapexuputian@126.com>
2023-10-22 19:35:35 -0400

    11

---
+++ wk6_Studio/Assets/scripts/playerMoving.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMoving : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    private float moveInput;
+    private
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
