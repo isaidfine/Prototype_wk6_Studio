7ebe2c88726a76b90d9eb4407025543bbd9d6efa
PrisicllaX <grapexuputian@126.com>
2023-10-23 02:16:25 -0400

    PlatformChanging

--- wk6_Studio/Assets/scripts/GameStatus.cs
+++ wk6_Studio/Assets/scripts/GameStatus.cs
@@ -6,7 +6,7 @@ using UnityEngine;
     public struct WorldSprite
     {
         public Sprite playerSprite;
-        public GameObject platformObject;
+        public GameObject platformObjects;
         public Sprite bgSprite;       
     }
 
@@ -23,7 +23,7 @@ public class GameStatus : MonoBehaviour
 
     public float floatingTime;
     public bool isChanging;
-    public float t = 0f;
+    private float t = 0f;
     public GameObject currentPlayer;
     public GameObject formerPlayer;
     public GameObject currentBG;
@@ -32,12 +32,20 @@ public class GameStatus : MonoBehaviour
     private Color startColor ;
     private Color endColor ;
 
-    private List<GameObject> GrammaPlatformList;
-    private List<GameObject> RabbitPlatformList;
-    private List<GameObject> SnakePlatformList;
+    private GameObject currentPlatform;
+    private GameObject formerPlatform;
+    private List<GameObject> formerPlatformList = new List<GameObject>();
+    private List<GameObject> currentPlatformList = new List<GameObject>() ;
     
     void Start()
     {
+        currentPlatform = GrammaSprites.platformObjects;
+        formerPlatform = SnakeSprites.platformObjects;
+        Debug.Log(currentPlatform);
+        // SetupChildren(currentPlatform.transform,currentPlatformList);
+        // List<GameObject> currentPlatformList = new List<GameObject>();
+        // List<GameObject> formerPlatformList = new List<GameObject>();
+        currentPlatform.SetActive(true);
         startColor = new Color(1,1,1,0);
         endColor = new Color(1,1,1,1);
     }
@@ -59,6 +67,7 @@ public class GameStatus : MonoBehaviour
         formerPlayer.GetComponent<SpriteRenderer>().color = new Color(1,1,1,1);
         currentBG.GetComponent<SpriteRenderer>().color = new Color(1,1,1,0);
         currentPlayer.GetComponent<SpriteRenderer>().color = new Color(1,1,1,0);
+
         switch (currentStatus)
         {
             case WorldStatus.Gramma:
@@ -67,6 +76,11 @@ public class GameStatus : MonoBehaviour
                 formerBG.GetComponent<SpriteRenderer>().sprite = GrammaSprites.bgSprite;
                 formerPlayer.GetComponent<SpriteRenderer>().sprite = GrammaSprites.playerSprite;
                 currentStatus = WorldStatus.Rabbit;
+                currentPlatform =RabbitSprites.platformObjects;
+                formerPlatform = GrammaSprites.platformObjects;
+                // RabbitSprites.platformObjects.SetActive(true);
+                // UpdateChildren(GrammaSprites.platformObjects.transform, formerPlatformList);
+                // UpdateChildren(RabbitSprites.platformObjects.transform, currentPlatformList);
                 break;
 
             case WorldStatus.Rabbit:
@@ -75,6 +89,11 @@ public class GameStatus : MonoBehaviour
                 formerBG.GetComponent<SpriteRenderer>().sprite = RabbitSprites.bgSprite;
                 formerPlayer.GetComponent<SpriteRenderer>().sprite = RabbitSprites.playerSprite;
                 currentStatus = WorldStatus.Snake;
+                currentPlatform = SnakeSprites.platformObjects;
+                formerPlatform = RabbitSprites.platformObjects;
+                // SnakeSprites.platformObjects.SetActive(true);
+                // UpdateChildren(SnakeSprites.platformObjects.transform, currentPlatformList);
+                // UpdateChildren(RabbitSprites.platformObjects.transform, formerPlatformList);
                 break;
 
             case WorldStatus.Snake:
@@ -83,9 +102,18 @@ public class GameStatus : MonoBehaviour
                 formerBG.GetComponent<SpriteRenderer>().sprite = SnakeSprites.bgSprite;
                 formerPlayer.GetComponent<SpriteRenderer>().sprite = SnakeSprites.playerSprite;
                 currentStatus = WorldStatus.Gramma;
+                currentPlatform = GrammaSprites.platformObjects;
+                formerPlatform = SnakeSprites.platformObjects;
+                // GrammaSprites.platformObjects.SetActive(true);
+                // UpdateChildren(GrammaSprites.platformObjects.transform, currentPlatformList);
+                // UpdateChildren(SnakeSprites.platformObjects.transform, formerPlatformList);
                 break;
             
         }
+        currentPlatform.SetActive(true);
+
+        UpdateChildren(currentPlatform.transform, currentPlatformList);
+        UpdateChildren(formerPlatform.transform, formerPlatformList);
     }
 
     void ChangingStatus()
@@ -98,18 +126,50 @@ public class GameStatus : MonoBehaviour
             formerPlayer.GetComponent<SpriteRenderer>().color = Color.Lerp(startColor, endColor, 1-t);
             currentPlayer.GetComponent<SpriteRenderer>().color = Color.Lerp(startColor,endColor,t);
             currentBG.GetComponent<SpriteRenderer>().color = Color.Lerp(startColor,endColor,t);
+            foreach(GameObject platform in currentPlatformList)
+            {
+                platform.transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.Lerp(startColor, endColor,t);
+            }
+            foreach(GameObject platform in formerPlatformList)
+            {
+                platform.transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.Lerp(startColor, endColor,1-t);
+            }
+
+
         }
         else
         {
             isChanging = false;
+            // switch(currentStatus)
+            // {
+            //     case WorldStatus.Gramma:
+            //     SnakeSprites.platformObjects.SetActive(false);
+            //     break;
+            //     case WorldStatus.Rabbit:
+            //     GrammaSprites.platformObjects.SetActive(false);
+            //     break;
+            //     case WorldStatus.Snake:
+            //     RabbitSprites.platformObjects.SetActive(false);
+            //     break;
+            // }
+            formerPlatform.SetActive(false);
             formerBG.SetActive(false);
             formerPlayer.SetActive(false);
         }
     }
 
-    void GetDirectChildren(Transform parent, List<GameObject> directChildren)
+    void UpdateChildren(Transform parent, List<GameObject> directChildren)
     {
         directChildren.Clear();
+        for (int i = 0; i < parent.childCount; i++)
+        {
+            Transform child = parent.GetChild(i);
+            directChildren.Add(child.gameObject);
+        }
+    }
+
+    void SetupChildren(Transform parent, List<GameObject> directChildren)
+    {
         for (int i = 0; i < parent.childCount; i++)
         {
             Transform child = parent.GetChild(i);
